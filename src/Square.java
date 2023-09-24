1b1749da21bab7846c4077bf1d4c7a6d21fd63af
neymaru <javaxkim@naver.com>
2023-09-24 23:34:27 +0900

    while loop Square

---
+++ src/Square.java
@@ -0,0 +1,10 @@
+public class Square {
+    public static void main(String[] args) {
+        // while 반복문 사용해 높이가 1000, 너비가 10인 사각형 출력하기
+        int i = 0;
+        while(i < 1000) {
+            System.out.println("**********");
+            i += 1;
+        }
+    }
+}
