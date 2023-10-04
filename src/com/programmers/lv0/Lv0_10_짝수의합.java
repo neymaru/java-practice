6f199b790d9fcc9fc3a53c49c5b71c0c77399f09
neymaru <javaxkim@naver.com>
2023-10-04 23:45:38 +0900

    Java 코딩테스트 1문제

---
+++ src/com/programmers/lv0/Lv0_10_짝수의합.java
@@ -0,0 +1,18 @@
+package com.programmers.lv0;
+
+public class Lv0_10_짝수의합 {
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 1; i <= n; i += 1) {
+            if (i % 2 == 0) answer += i;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+    Lv0_10_짝수의합 result = new Lv0_10_짝수의합();
+        System.out.println(result.solution(10));
+        System.out.println(result.solution(4));
+    }
+
+}
