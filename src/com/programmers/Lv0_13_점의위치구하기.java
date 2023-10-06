a24d3069a3bb27451dbcaf9297765e134ab618ba
neymaru <javaxkim@naver.com>
2023-10-06 23:54:35 +0900

    Java 코딩테스트 2문제

---
+++ src/com/programmers/Lv0_13_점의위치구하기.java
@@ -0,0 +1,13 @@
+package com.programmers;
+
+public class Lv0_13_점의위치구하기 {
+    public int solution(int[] dot) {
+        return (dot[0] > 0 ? (dot[1] > 0 ? 1 : 4) : dot[1] > 0 ? 2 : 3);
+    }
+
+    public static void main(String[] args) {
+        Lv0_13_점의위치구하기 result = new Lv0_13_점의위치구하기();
+        System.out.println(result.solution(new int[]{2, 4}));
+        System.out.println(result.solution(new int[]{-7, 9}));
+    }
+}
